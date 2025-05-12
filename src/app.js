2ff7b40543fde796e5e8f4b16c2eedf33aed2b42
“Anzhela <anzhela.iondem@gamil.com>
2025-05-12 08:54:44 -0400

    Change the app welcome text

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000
 app.get('/', (req, res)=>{
     res.status(200);
-    res.send("Welcome to root URL of Server");
+    res.send("Welcome to the Root URL of Server");
 });
 
 app.listen(PORT, (error) =>{
