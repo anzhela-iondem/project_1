f7ef857b728e04d07c5dfd29722d9808d76f510e
“Anzhela <anzhela.iondem@gamil.com>
2025-05-13 07:45:24 -0400

    Change .sh file

--- apply_terraform.sh
+++ apply_terraform.sh
@@ -4,10 +4,9 @@ set -e
 
 cd ./terraform
 terraform init
-#terraform apply -var-file=dev.tfvars -auto-approve
-terraform destroy -var-file=dev.tfvars -auto-approve
+terraform apply -var-file=dev.tfvars -auto-approve
+#terraform destroy -var-file=dev.tfvars -auto-approve
 
 # source .env
 # aws eks update-kubeconfig --region $region --name $cluster_name
-
-export KUBECONFIG=$(pwd)/kubeconfig.yaml
+#export KUBECONFIG=$(pwd)/kubeconfig.yaml
