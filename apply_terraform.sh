1ab87417ed8b7232ef5c26cbb46a434ade0c65b9
“Anzhela <anzhela.iondem@gamil.com>
2025-05-13 17:09:56 -0400

    Add cd.yml, refine ci.yml

--- apply_terraform.sh
+++ apply_terraform.sh
@@ -3,9 +3,9 @@
 set -e
 
 cd ./terraform
-terraform init
-terraform apply -var-file=dev.tfvars -auto-approve
-#terraform destroy -var-file=dev.tfvars -auto-approve
+#terraform init
+#terraform apply -var-file=dev.tfvars -auto-approve
+terraform destroy -var-file=dev.tfvars -auto-approve
 
 # source .env
 # aws eks update-kubeconfig --region $region --name $cluster_name
