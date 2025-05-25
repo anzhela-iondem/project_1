bcd83072adb2b53da213dedefc5dace151df41b7
“Anzhela <anzhela.iondem@gamil.com>
2025-05-25 16:26:18 -0400

    Add ecr url.

--- apply_terraform.sh
+++ apply_terraform.sh
@@ -4,8 +4,8 @@ set -e
 
 cd ./terraform
 #terraform init
-#terraform apply -var-file=dev.tfvars -auto-approve
-terraform destroy -var-file=dev.tfvars -auto-approve
+terraform apply -var-file=dev.tfvars -auto-approve
+#terraform destroy -var-file=dev.tfvars -auto-approve
 
 source .env
 aws eks update-kubeconfig --region $region --name $cluster_name
